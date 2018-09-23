d31ebf277a8a8332f391ee2561724fdff6b5a760
Todd <mopeybear@gmail.com>
2018-09-23 12:50:57 -0400

    MainMenu & Options tree built

--- src/main/java/shelterApp/ShelterApp.java
+++ src/main/java/shelterApp/ShelterApp.java
@@ -14,9 +14,63 @@ public class ShelterApp {
 		appStartUp();
 		System.out.println(ShelterAppMenus.welcome());
 		
+		
+		do {
+			
 		System.out.println(ShelterAppMenus.mainMenu());
+		String userMainMenuInput = input.nextLine();
 		
+		if (userMainMenuInput.equalsIgnoreCase("F")) {
+			shelter.feedOrganicPets();
+			
+		} else if (userMainMenuInput.equalsIgnoreCase("D")) {
+			shelter.giveWaterToOrganicPets();
+			
+		} else if (userMainMenuInput.equalsIgnoreCase("O")) {
+			shelter.oilRoboticPets();
+			
+		} else if (userMainMenuInput.equalsIgnoreCase("W")) {
+			shelter.walkDogs();
+			
+		} else if (userMainMenuInput.equalsIgnoreCase("L")) {
+			shelter.playLasersWithPets();
 		
+		} else if (userMainMenuInput.equalsIgnoreCase("M")) {
+			shelter.playWithToyMice();
+			
+		} else if (userMainMenuInput.equalsIgnoreCase("CB")) {
+			shelter.cleanLitterBox();
+			
+		} else if (userMainMenuInput.equalsIgnoreCase("CC")) {
+			shelter.cleanAllDogCages();
+			
+		} else if (userMainMenuInput.equalsIgnoreCase("V")) {
+			//call name of pet
+			String petToCallVet = input.nextLine();
+			shelter.callVet(petToCallVet);
+			
+		} else if (userMainMenuInput.equalsIgnoreCase("B")) {
+			//call name of Pet
+			String petToChangeBatteries = input.nextLine();
+			shelter.changeBatteries(petToChangeBatteries);
+			
+		} else if (userMainMenuInput.equalsIgnoreCase("T")) {
+			//code to take in new pet into shelter
+			
+		} else if (userMainMenuInput.equalsIgnoreCase("A")) {
+			//call name of pet
+			String petToAdopt = input.nextLine();
+			shelter.adopt(petToAdopt);
+			
+		} else if (userMainMenuInput.equalsIgnoreCase("Q")) {
+			break;
+		} else {
+			System.out.println("Invalid Input");
+		}
+		
+		shelter.tickAll();
+		
+		} while (true);
 		
 		
 		System.out.println(ShelterAppMenus.closing());
--- src/main/java/shelterApp/ShelterAppMenus.java
+++ src/main/java/shelterApp/ShelterAppMenus.java
@@ -1,42 +1,35 @@
 package shelterApp;
 
+import pets.PetShelter;
+
 public class ShelterAppMenus {
 
 	public static String welcome() {
 		String welcome = "welcome placeholder";
 		return welcome;
 	}
+	
+	public static String petStatus(PetShelter shelter) {
+		String petStatus = "Placeholder";
+		
+		return petStatus;
+	}
 
 	public static String mainMenu() {
-		String mainMenu = "Options Menu\n" 
+		String mainMenu = "\t\t\t-----Options Menu-----\n" 
 	+ "Press \'F\' to FEED the pets\t\t" 
 				+ "Press \'D\' to give water to DRINK\n"
 	+ "Press \'O\' to OIL pets\t\t\t"
 				+ "Press \'W\' to WALK all the dogs\n" 
 	+ "Press \'L\' to play with LASER pointers\t"
-				+ "Press \'M\' to play with fake MICE\n\n"
+				+ "Press \'M\' to play with fake MICE\n\t\t\t  ------------------\n"
 	+ "Press \'CB\' to CLEAN litterbox\t\t" 
 				+ "Press \'CC\' to CLEAN Cages\n"
 	+ "Press \'V\' to Call VET \t\t\t" 
-				+ "Press \'B\' to Change BATTERIES\n\n"
+				+ "Press \'B\' to Change BATTERIES\n\t\t\t  ------------------\n"
 	+ "Press \'T\' to TAKE in a new pet\t\t" 
-				+ "Press \'A\' to have a pet ADOPTed";
-		/*
-		 * All options Feed Organics Give Water To Organics Oil Robots
-		 * 
-		 * Walk Dogs Bring out laser pens Bring out toy mice
-		 * 
-		 * 
-		 * 
-		 * 
-		 * Clean Cages Clean LitterBox
-		 * 
-		 * Call Vet Change Batteries
-		 * 
-		 * Adopt Take in new pet
-		 * 
-		 * 
-		 */
+				+ "Press \'A\' to have a pet ADOPTed\n"
+	+ "\t\t  ---Press \'Q\' to QUIT application---\n";
 
 		return mainMenu;
 	}
